704783485a2da2d9cc830d80510d40e2c4a8fe4d
Jansen Fink <jansenfink@charter.net>
2020-07-10 19:12:27 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot 
-  puts "Squawk!"
-  return
+
+def parrot(phrase)
+  phrase = "Squawk!"
+  puts phrase
+  return phrase
 end 
